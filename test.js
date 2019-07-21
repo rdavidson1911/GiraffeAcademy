bd036a96f62b888fbe2c6345dae8632c3c648e2c
rdavidson1911 <rdavidson1911@gmail.com>
2019-07-20 21:59:20 -0500

    test

---
+++ test.js
@@ -0,0 +1,15 @@
+console.log("Hello World");
+alert("Robert is awesome!");
+
+
+let a = +prompt('a?', "");
+
+switch (a) {
+    case 0:
+        alert("A is 0");
+    case 1:
+        alert("A is 1");
+    case 2:
+    case 3:
+        alert("A is 2 or 3");
+}
