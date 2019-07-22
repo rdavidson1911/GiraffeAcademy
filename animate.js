51e07c2aa9e60655f6f9dde7f2eb80301ff27540
rdavidson1911 <rdavidson1911@gmail.com>
2019-07-21 20:57:42 -0500

    hhh

--- animate.js
+++ animate.js
@@ -5,12 +5,28 @@ var pos = 0;
 var box = document.getElementById("box");
 var t = setInterval(move, 10);
 
+var hitwall = false;
 function move() {
-  if(pos >= 150) {
-    clearInterval(t);
-  }
-  else {
+  if(pos <= 149 && hitwall == false) {
     pos += 1;
     box.style.left = pos+"px";
   }
+
+  if(pos = 149) {
+      hitwall = true;
+  }
+
+  if(hitwall == true){
+    pos -= 1;
+    box.style.left = pos+"px";
+
+  }
 }
+  
+
+
+
+
+    
+
+
